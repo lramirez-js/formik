2c7a45e7f32c9ed041b034b6f1b1b15b70ed9af2
lramirez-js <luis.rg@outlook.com>
2022-08-26 04:24:49 -0500

    Validation added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import { useFormik } from 'formik'
 
+const validate = (values) => {
+  const errors = {}
+
+  if(!values.name) {
+    errors.name = 'Required'
+  } else if(values.name.length < 5) {
+    errors.name = 'Name is too short'
+  }
+
+  if(!values.lastname) {
+    errors.lastname = 'Required'
+  } else if(values.lastname.length < 5) {
+    errors.lastname = 'Lastname is too short'
+  }
+
+  return errors
+}
+
 function App() {
   const formik = useFormik({
     initialValues: {
@@ -7,6 +25,7 @@ function App() {
       lastname: '',
       email: ''
     },
+    validate,
     onSubmit: values => {
       console.log(values)
     }
@@ -21,12 +40,18 @@ function App() {
         onChange={formik.handleChange}
         value={formik.values.name}
       />
+      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      <br />
+      <label>Lastname</label>
       <input 
         name='lastname'
         type='text'
         onChange={formik.handleChange}
         value={formik.values.lastname}
       />
+      {formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
+      <br />
+      <label>Email</label>
       <input 
         name='email'
         type='email'
